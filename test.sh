5768158655853191d144a6117b7ad7f4100a0ac4
crs <crs@theswan>
2018-06-11 17:22:57 +0200

    remove certain things and added stuff

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cwd=`pwd`
+
+
 cd /tmp
 mkdir mybenchmark
 cd mybenchmark
@@ -8,4 +11,4 @@ python3 -m venv testenv
 . /tmp/mybenchmark/testenv/bin/activate
 
 pip install numpy==1.14.4 networkx==2.1
-python /tmp/foo/benchmarks.py
+python $cwd/benchmarks.py
