fcfe08cce170a8da6a1e60e148db7e29aea35025
crs <crs@theswan>
2018-06-11 18:32:13 +0200

    fixed shit that havent been fixed in last commit

--- benchmarks.py
+++ benchmarks.py
@@ -7,32 +7,46 @@ def printres(exname, r):
     a = np.array(r)
     print("{:20s} mean {:8.3f} (sd{:8.3f})".format(exname, a.mean(),a.std()))
 
+def run_benchmark(benchmarkname, stmt, setup, niter):
+    t = timeit.Timer(stmt, setup=setup)
+    res = [t.timeit(1) for x in range(niter)]
+    printres(benchmarkname, res)  
+
 print("""
 python version {:s}
 numpy version {:s}
 networkx version {:s}
 """.format(sys.version.replace('\n', ""), np.version.version, nx.__version__))
 
+dim = 4096
+n,m = 100,10 # parameter for BA graph
 
-npsetup = """
+niter = 5
+
+
+# matrix mult
+run_benchmark('np dot',
+	"A = np.dot(A,A)",
+	"""
 import numpy as np
-
-dim = 5000
-A = np.random.random((dim,dim))
-"""
-
-nptimer = timeit.Timer("A = np.dot(A,A)", setup=npsetup)
-npres = [nptimer.timeit(1) for x in range(5)]
-
-printres("np dot", npres)
+A = np.random.random(({dim},{dim}))
+""".format(dim=dim),
+niter)
 
 
-nxtimer = timeit.Timer('nx.algorithms.betweenness_centrality(g)', setup="""
+run_benchmark('np svd', "np.linalg.svd(A, full_matrices = False)",
+	"""
+import numpy as np
+A = np.random.random((int({size} / 2), int({size} / 4)))
+""".format(size=dim),
+niter)
+
+
+run_benchmark('nx betweenness', 'nx.algorithms.betweenness_centrality(g)',
+	"""
 import networkx as nx
-N = 500
-g = nx.barabasi_albert_graph(N, 10)
-""")
+g = nx.barabasi_albert_graph({n}, {m})
+""".format(n=n, m=m),
+niter)
 
-nxres = [nxtimer.timeit(1) for x in range(5)]
 
-printres("nx betweenness", nxres)
